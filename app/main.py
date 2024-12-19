87074896bd9f82f358c979a9e913442ca022d66c
allanh <joelshn123@gmail.com>
2024-12-18 19:46:49 -0600

    puerto

--- app/main.py
+++ app/main.py
@@ -93,4 +93,4 @@ def show_email():
     return render_template_string(html_template)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=8080)
+    app.run(debug=True,host='0.0.0.0', port=80)
